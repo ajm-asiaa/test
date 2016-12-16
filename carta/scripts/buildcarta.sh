9672c28a55d01716fd4790747afce97e933bae55
Grimmer Kang <grimmer0125@gmail.com>
2016-12-16 23:55:23 +0800

    Try to build desktop version only for CircleCI

--- carta/scripts/buildcarta.sh
+++ carta/scripts/buildcarta.sh
@@ -8,8 +8,8 @@ HOME=/home/developer
 export PATH=$PATH:$HOME/Qt/5.3/gcc_64/bin
 
 ln -s $HOME/src/CARTAvis-externals/ThirdParty/ast-8.0.2 $HOME/src/CARTAvis-externals/ThirdParty/ast
-ln -s $HOME/src/CARTAvis-externals/ThirdParty/casacore-2.10.2016 $HOME/src/CARTAvis-externals/ThirdParty/casacore 
-ln -s $HOME/src/CARTAvis-externals/ThirdParty/cfitsio-shared $HOME/src/CARTAvis-externals/ThirdParty/cfitsio 
+ln -s $HOME/src/CARTAvis-externals/ThirdParty/casacore-2.10.2016 $HOME/src/CARTAvis-externals/ThirdParty/casacore
+ln -s $HOME/src/CARTAvis-externals/ThirdParty/cfitsio-shared $HOME/src/CARTAvis-externals/ThirdParty/cfitsio
 ln -s $HOME/src/CARTAvis-externals/ThirdParty/imageanalysis-2.10.2016 $HOME/src/CARTAvis-externals/ThirdParty/imageanalysis
 
 cp $HOME/src/CARTAvis/carta/scripts/runScriptedClientTests.sh $HOME
@@ -24,7 +24,7 @@ cd $HOME/src/CARTAvis/carta/html5/common/skel
 
 mkdir $HOME/src/build
 cd $HOME/src/build
-qmake  CARTA_BUILD_TYPE=dev $HOME/src/CARTAvis/carta -r
+qmake noserver=1 CARTA_BUILD_TYPE=dev $HOME/src/CARTAvis/carta -r
 try make
 find . -name "*.o" -exec rm -f {} \;
 find . -name "Makefile" -exec rm -f {} \;
